a1e6b0db5a6d8e47ca2a14807ea8496434c013d7
avi <avikan1087@gmail.com>
2020-01-31 13:26:05 -0500

    done

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,8 +1,21 @@
+const jwt = require("jsonwebtoken");
+const secrets = require('../secret/secrets');
 /* 
   complete the middleware code to check if the user is logged in
   before granting access to the next middleware/route handler
 */
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  const token = req.headers.authorization;
+
+  if (token) {
+      jwt.verify(token, secrets.jwtSecret, (error, tokenData) => {
+      if (error){
+          res.status(401).json({ message: "Invalid token"})
+      } else {
+          req.username = tokenData.username;
+          next();
+      }
+      })
+  }
 };
--- knexfile.js
+++ knexfile.js
@@ -9,4 +9,30 @@ module.exports = {
     },
     seeds: { directory: './database/seeds' },
   },
+  pool: {
+    afterCreate: (conn, done) => {
+      conn.run("PRAGMA foreign_keys = ON", done);
+    },
+    
+  },
+
+  testing: {
+    client: "sqlite3",
+    connection: {
+      filename: "./database/test.db3",
+    },
+    useNullAsDefault: true,
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done);
+      },
+    },
+    migrations: {
+      directory: "./database/migrations",
+    },
+    seeds: {
+      directory: "./database/seeds",
+    },
+  },
+  
 };
